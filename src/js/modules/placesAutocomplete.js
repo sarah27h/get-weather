c3a08fea0c317ede36795e4f0ff546989453eedc
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2020-09-09 10:39:43 +0200

    Refactor: add more option to autocomplete

--- src/js/modules/placesAutocomplete.js
+++ src/js/modules/placesAutocomplete.js
@@ -1,8 +1,8 @@
 import { PLACES_APP_ID, PLACES_API_KEY } from '../../../config/config';
 import fetchTempData from './fetchTemp.js';
+import showMessage from './showMessages';
 
 // const places = require('places.js');
-// import places from 'places.js';
 // import places from 'places.js';
 
 const initAutocomplete = (() => {
@@ -10,10 +10,19 @@ const initAutocomplete = (() => {
     appId: PLACES_APP_ID,
     apiKey: PLACES_API_KEY,
     container: document.querySelector('#autocomplete-search')
+  }).configure({
+    // Restrict the search results to city
+    type: 'city'
+    // aroundLatLngViaIP: false,
   });
-  placesAutocomplete.on('change', e =>
-    fetchTempData(e.suggestion.latlng.lat, e.suggestion.latlng.lng)
-  );
+  placesAutocomplete.on('change', e => {
+    fetchTempData(e.suggestion.latlng.lat, e.suggestion.latlng.lng);
+    // clear search input after submit to improve UX
+    placesAutocomplete.setVal('');
+    console.log(e.suggestion);
+    showMessage(`${e.suggestion.name}, ${e.suggestion.country}`);
+  });
+  placesAutocomplete.on('error', e => console.log(e.message));
 })();
 
 console.log(initAutocomplete);
